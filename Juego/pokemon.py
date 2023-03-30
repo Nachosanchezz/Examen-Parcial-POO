7b5182f28237901593d65fc6e8089404c389fb90
Nacho <isanccal@myuax.com>
2023-03-30 20:27:07 +0200

    Cambio

--- Juego/pokemon.py
+++ Juego/pokemon.py
@@ -1,75 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-"""
-This Python module contains not only the class Pokemon, but also the test of
-this Python class.
-
-@contents :  This module contains not only a single Python class, but also the
-             test cases to probe its functionality.
-@project :  N/A
-@program :  N/A
-@file :  pokemon.py
-@author :  Antonio Artes Garcia (antonio.artesgarcia@ceu.es)
-           Francisco Hernando Gallego (francisco.hernandogallego@ceu.es)
-           Ruben Juarez Cadiz (ruben.juarezcadiz@ceu.es)
-
-@version :  0.0.1, 08 November 2021
-@information :  The Zen of Python
-                  https://www.python.org/dev/peps/pep-0020/
-                Style Guide for Python Code
-                  https://www.python.org/dev/peps/pep-0008/
-                Example NumPy Style Python Docstrings
-                  http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_numpy.html
-                doctest – Testing through documentation
-                  https://pymotw.com/2/doctest/
-
-@copyright :  Copyright 2021 GNU AFFERO GENERAL PUBLIC.
-              All rights are reserved. Reproduction in whole or in part is
-              prohibited without the written consent of the copyright owner.
-"""
-
-
 # Source packages.
+from Juego.weapon_type import WeaponType
 
-from pokemon import Pokemon
-from weapon_type import WeaponType
-import random
-
-class PokemonWater():
-    """Python class to implement a basic version of a Pokemon of the game.
-
-    This Python class implements the basic version of a Pokemon of the game.
-
-    Syntax
-    ------
-      obj = PokemonWater(id, pokemon_name, weapon_type, health_points,
-                         attack_rating, defense_rating)
-
-    Parameters
-    ----------
-      [in] id ID of the Pokemon.
-      [in] pokemon_name Name of the Pokemon.
-      [in] weapon_type Type of weapon that carries out the Pokemon.
-      [in] health_points Points of health that the Pokemon has.
-      [in] attack_rating Attack rating of the Pokemon.
-      [in] defense_rating Defense rating of the Pokemon.
-
-    Returns
-    -------
-      obj Python object output parameter that represents an instance
-          of the class Pokemon.
-
-    Attributes
-    ----------
-
-    Example
-    -------
-      >>> from pokemon import Pokemon
-      >>> from weapon_type import WeaponType
-      >>> obj_Pokemon = PokemonWater(1, "Squirtle", WeaponType.PUNCH, 100, 7, 10)
-    """
+class Pokemon():
 
     def __init__ (self, id, pokemon_name, weapon_type, health_points,
                   attack_rating, defense_rating):
@@ -78,12 +10,8 @@ class PokemonWater():
         self.pokemon_name = pokemon_name
         self.weapon_type = weapon_type
         self.health_points = int(health_points)
-        self.defense_rating = int(defense_rating)
         self.attack_rating = int(attack_rating)
-        if attack_rating is not None:
-            self.attack_rating = attack_rating
-        else:
-            self.attack_rating = random.randint(11, 20)
+        self.defense_rating = int(defense_rating)
     def get_pokemon_name(self):
         return self.pokemon_name
     def get_weapon_type(self):
@@ -112,6 +40,7 @@ class PokemonWater():
             return True
 
 
+
 def main():
     """Function main of the module.
 
@@ -138,9 +67,9 @@ def main():
     print("=================================================================.")
     print("Test Case 1: Create a Pokemon.")
     print("=================================================================.")
-    pokemon_1 = PokemonWater(1, "Squirtle", WeaponType.HEADBUTT, 100, 12, 8)
+    pokemon_1 = Pokemon(1, "Ivysaur", WeaponType.HEADBUTT, 100, 8, 9)
 
-    if pokemon_1.get_pokemon_name() == "Squirtle":
+    if pokemon_1.get_pokemon_name() == "Ivysaur":
         print("Test PASS. The parameter pokemon_name has been correctly set.")
     else:
         print("Test FAIL. Check the method __init__().")
@@ -155,12 +84,12 @@ def main():
     else:
         print("Test FAIL. Check the method __init__().")
 
-    if pokemon_1.get_attack_rating() == 12:
+    if pokemon_1.get_attack_rating() == 8:
         print("Test PASS. The parameter attack_rating has been correctly set.")
     else:
         print("Test FAIL. Check the method __init__().")
 
-    if pokemon_1.get_defense_rating() == 8:
+    if pokemon_1.get_defense_rating() == 9:
         print("Test PASS. The parameter defense_rating has been correctly set.")
     else:
         print("Test FAIL. Check the method __init__().")
@@ -169,9 +98,9 @@ def main():
     print("=================================================================.")
     print("Test Case 2: Human-readable format of the object.")
     print("=================================================================.")
-    pokemon_2 = PokemonWater(7, "Squirtle", WeaponType.HEADBUTT, 100,15, 7)
+    pokemon_2 = Pokemon(2, "Charmander", WeaponType.HEADBUTT, 100, 7, 10)
 
-    if str(pokemon_2) == "Pokemon ID 7 with name Squirtle has as weapon HEADBUTT and health 100":
+    if str(pokemon_2) == "Pokemon ID 2 with name Charmander has as weapon HEADBUTT and health 100":
         print("Test PASS. The human-readable format of the object has been implemented correctly.")
     else:
         print("Test FAIL. Check the method __str__()." + " RESULT: " + str(pokemon_2))
@@ -180,7 +109,7 @@ def main():
     print("=================================================================.")
     print("Test Case 3: Pokemon alive?¿?.")
     print("=================================================================.")
-    pokemon_3 = PokemonWater(3, "Squirtle", WeaponType.KICK, 97, 15, 8)
+    pokemon_3 = Pokemon(3, "Wartortle", WeaponType.KICK, 97, 8, 9)
 
     if pokemon_3.is_alive():
         pokemon_3.fight_defense(200)  # With this the Pokemon should be retired.
@@ -196,11 +125,11 @@ def main():
     print("=================================================================.")
     print("Test Case 4: Check the defense during a Fight.")
     print("=================================================================.")
-    pokemon_4 = PokemonWater(4, "Squirtle", WeaponType.ELBOW, 93, 11, 9)
+    pokemon_4 = Pokemon(4, "Squirtle", WeaponType.ELBOW, 93, 9, 6)
 
     pokemon_4.fight_defense(70)
 
-    if pokemon_4.get_health_points() == 32:
+    if pokemon_4.get_health_points() == 29:
         print("Test PASS. The method fight_defense() has been implemented correctly.")
     else:
         print("Test FAIL. Check the method fight_defense().")
@@ -209,13 +138,13 @@ def main():
     print("=================================================================.")
     print("Test Case 5: Check the attack during a Fight.")
     print("=================================================================.")
-    pokemon_5 = PokemonWater(5, "Squirtle", WeaponType.PUNCH, 99, 20, 10)
-    pokemon_6 = PokemonWater(6, "Squirtle", WeaponType.PUNCH, 99, 18, 9)
+    pokemon_5 = Pokemon(5, "Venusaur", WeaponType.PUNCH, 99, 10, 7)
+    pokemon_6 = Pokemon(6, "Charmeleon", WeaponType.PUNCH, 99, 9, 8)
 
     pokemon_was_hit = pokemon_5.fight_attack(pokemon_6)
 
     if pokemon_was_hit:
-        if pokemon_6.get_health_points() == 88:
+        if pokemon_6.get_health_points() == 97:
             print("Test PASS. The method fight_attack() has been implemented correctly.")
         else:
             print("Test FAIL. Check the method fight_attack().")
