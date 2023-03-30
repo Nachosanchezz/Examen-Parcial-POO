5bdaaa9d60fb4357edd96b62ead3c882fe907efb
Nacho <isanccal@myuax.com>
2023-03-30 20:27:01 +0200

    Cambio

---
+++ Juego/main.py
@@ -0,0 +1,79 @@
+import csv
+from Juego.pokemon import Pokemon
+
+
+def get_data_from_coach(coach_1_pokemons, csv):
+    pokemons_coach_1 = []
+    with open("coach_1_pokemons.csv", "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            id = row [0]
+            name = row [1]
+            weapon = row [2]
+            health = row [3]
+            attack = row [4]
+            defense = row [5]
+            p = Pokemon(id, name, weapon, health, attack, defense)
+            pokemons_coach_1.append(p)
+            
+    
+    pokemons_coach_2 = []
+    with open("coach_2_pokemons.csv", "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            id = row [0]
+            name = row [1]
+            weapon = row [2]
+            health = row [3]
+            attack = row [4]
+            defense = row [5]
+            p = Pokemon(id, name, weapon, health, attack, defense)
+            pokemons_coach_2.append(p)
+
+    return pokemons_coach_1, pokemons_coach_2
+
+def get_pokemon_in_a_list_of_pokemons(pokemon):
+    print("Choose a Pokemon from the list: ")
+    for i, p in enumerate(pokemon):
+        print(i, p.name)
+
+    while True:
+        try:
+            opcion = int(input("Choose a Pokemon: "))
+            if opcion < 0 or opcion > len(pokemon):
+                print("The option is not valid. Try again.")
+            else:
+                return pokemon[opcion - 1]
+        except ValueError:
+            print("The option is not valid. Try again.")
+
+def coach_is_undefeated(list_of_pokemons):
+    for p in list_of_pokemons:
+        if p.health > 0:
+            return False
+    return True
+
+def main():
+    coach1 = get_data_from_coach("coach_1_pokemons.csv")
+    coach2 = get_data_from_coach("coach_2_pokemons.csv")
+
+    coach_playing1 = coach1[0]
+    coach_playing2 = coach2[0]
+
+    print("The coach 1 has chosen " + coach_playing1.name)
+    print("The coach 2 has chosen " + coach_playing2.name)
+
+    while True:
+        if not coach_is_undefeated(coach1):
+            print("The coach 2 has won")
+            break
+        if not coach_is_undefeated(coach2):
+            print("The coach 1 has won")
+            break
+    
+    
+    if __name__ == "__main__":
+      main()
+
+
+
---
+++ Juego/pokemon_water.py
@@ -0,0 +1,169 @@
+# Source packages.
+from Juego.pokemon import Pokemon
+from Juego.weapon_type import WeaponType
+import random
+
+class PokemonWater():
+
+    def __init__ (self, id, pokemon_name, weapon_type, health_points,
+                  attack_rating, defense_rating):
+    
+        self.id = id
+        self.pokemon_name = pokemon_name
+        self.weapon_type = weapon_type
+        self.health_points = int(health_points)
+        self.defense_rating = int(defense_rating)
+        self.attack_rating = int(attack_rating)
+        if attack_rating is not None:
+            self.attack_rating = attack_rating
+        else:
+            self.attack_rating = random.randint(11, 20)
+    def get_pokemon_name(self):
+        return self.pokemon_name
+    def get_weapon_type(self):
+        return self.weapon_type
+    def get_health_points(self):
+        return self.health_points
+    def get_attack_rating(self):
+        return self.attack_rating
+    def get_defense_rating(self):
+        return self.defense_rating
+    def __str__(self):
+        return "Pokemon ID " + str(self.id) + " with name " + self.pokemon_name + " has as weapon " + self.weapon_type.name + " and health " + str(self.health_points)
+    def is_alive(self):
+        return self.health_points > 0
+    def fight_defense(self, damage):
+        self.health_points = self.health_points - damage + self.defense_rating
+        if self.health_points <= 0:
+            return False
+        else:
+            return True
+    def fight_attack(self, pokemon_attack):
+        self.health_points = self.health_points -  pokemon_attack.fight_defense(self.attack_rating)
+        if self.health_points <= 0:
+            return False
+        else:
+            return True
+
+
+def main():
+    """Function main of the module.
+
+    The function main of this module is used to test the Class that is described
+    in this module.
+
+    Syntax
+    ------
+      [ ] = main()
+
+    Parameters
+    ----------
+      Null .
+
+    Returns
+    -------
+      Null .
+
+    Example
+    -------
+      >>> main()
+    """
+
+    print("=================================================================.")
+    print("Test Case 1: Create a Pokemon.")
+    print("=================================================================.")
+    pokemon_1 = PokemonWater(1, "Squirtle", WeaponType.HEADBUTT, 100, 12, 8)
+
+    if pokemon_1.get_pokemon_name() == "Squirtle":
+        print("Test PASS. The parameter pokemon_name has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_weapon_type().name == "HEADBUTT":
+        print("Test PASS. The parameter weapon_type has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_health_points() == 100:
+        print("Test PASS. The parameter health_points has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_attack_rating() == 12:
+        print("Test PASS. The parameter attack_rating has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_defense_rating() == 8:
+        print("Test PASS. The parameter defense_rating has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+
+    print("=================================================================.")
+    print("Test Case 2: Human-readable format of the object.")
+    print("=================================================================.")
+    pokemon_2 = PokemonWater(7, "Squirtle", WeaponType.HEADBUTT, 100,15, 7)
+
+    if str(pokemon_2) == "Pokemon ID 7 with name Squirtle has as weapon HEADBUTT and health 100":
+        print("Test PASS. The human-readable format of the object has been implemented correctly.")
+    else:
+        print("Test FAIL. Check the method __str__()." + " RESULT: " + str(pokemon_2))
+
+
+    print("=================================================================.")
+    print("Test Case 3: Pokemon alive?¿?.")
+    print("=================================================================.")
+    pokemon_3 = PokemonWater(3, "Squirtle", WeaponType.KICK, 97, 15, 8)
+
+    if pokemon_3.is_alive():
+        pokemon_3.fight_defense(200)  # With this the Pokemon should be retired.
+
+        if not pokemon_3.is_alive():
+            print("Test PASS. The method is_alive() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method is_alive().")
+    else:
+        print("Test FAIL. Check the method is_alive().")
+
+
+    print("=================================================================.")
+    print("Test Case 4: Check the defense during a Fight.")
+    print("=================================================================.")
+    pokemon_4 = PokemonWater(4, "Squirtle", WeaponType.ELBOW, 93, 11, 9)
+
+    pokemon_4.fight_defense(70)
+
+    if pokemon_4.get_health_points() == 32:
+        print("Test PASS. The method fight_defense() has been implemented correctly.")
+    else:
+        print("Test FAIL. Check the method fight_defense().")
+
+
+    print("=================================================================.")
+    print("Test Case 5: Check the attack during a Fight.")
+    print("=================================================================.")
+    pokemon_5 = PokemonWater(5, "Squirtle", WeaponType.PUNCH, 99, 20, 10)
+    pokemon_6 = PokemonWater(6, "Squirtle", WeaponType.PUNCH, 99, 18, 9)
+
+    pokemon_was_hit = pokemon_5.fight_attack(pokemon_6)
+
+    if pokemon_was_hit:
+        if pokemon_6.get_health_points() == 88:
+            print("Test PASS. The method fight_attack() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_attack().")
+    else:
+        if pokemon_6.get_health_points() == 99:
+            print("Test PASS. The method fight_attack() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_attack().")
+
+
+
+# Checking whether this module is executed just itself alone.
+if __name__ == "__main__":
+    main()
+
+
+# EOF
