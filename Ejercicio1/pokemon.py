7d0258962760bf460504e2634361089485d9dc76
Nacho <isanccal@myuax.com>
2023-03-30 10:44:03 +0200

    Cambio1

---
+++ Ejercicio1/pokemon.py
@@ -0,0 +1,9 @@
+class Pokemon:
+    def __init__(self, ID, nombre, arma, salud, ataque, defensa):
+        self.ID = ID
+        self.nombre = nombre
+        self.arma = arma
+        self.salud = salud
+        self.ataque = ataque
+        self.defensa = defensa
+    
